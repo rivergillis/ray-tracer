56af2572ed4ab5f84533a895f2032480ec3e9fe0
River Gillis <jrivergillis@gmail.com>
2018-04-21 16:17:49 -0500

    Add ray init

--- hw6.cpp
+++ hw6.cpp
@@ -6,8 +6,10 @@
 #include "phong.h"
 
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
+#include <vector>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
 #ifdef MAC
 #include <GLUT/glut.h>
 #else
@@ -19,64 +21,84 @@ constexpr int kX = 800;
 constexpr int kY = 800;
 unsigned char image[kX][kY][3];
 
+// Single-dimension vector. Address with kX*[row] + col
+std::vector<Ray3D> rays;
 
-//---------------------------------------
-// Init function for OpenGL
-//---------------------------------------
-void init()
-{
-   // Initialize OpenGL
-   glClearColor(0.0, 0.0, 0.0, 1.0);
+// Projection plane is at (0, 0, 0) on Z = 0
+constexpr int kProjectionPlaneDistance = 0;
 
-   // Print command menu
-   std::cout << "Program commands:\n"
-        << "   'q' - quit program\n";
+// The camera is at (0,0,-distance)
+int distance = 8;
+
+std::pair<int, int> ImgToWorld(int img_x, int img_y) {
+  // middle of image is (0,0,0) in world coordinates
+  // so shift everything left by k[X/Y]/2
+  int world_x = img_x - kX/2;
+  int world_y = img_y - kY/2;
+  return std::make_pair(world_x, world_y);
 }
 
-//---------------------------------------
-// Display callback for OpenGL
-//---------------------------------------
-void display()
-{
-   // TO BE ADDED
- 
-   // Display image
-   glClear(GL_COLOR_BUFFER_BIT);
-   glDrawPixels(kX, kY, GL_RGB, GL_UNSIGNED_BYTE, image);
-   glFlush();
+void init() {
+  // Initialize OpenGL
+  glClearColor(0.0, 0.0, 0.0, 1.0);
+
+  // Print command menu
+  std::cout << "Program commands:\n"
+    << "   'q' - quit program\n";
 }
 
-//---------------------------------------
-// Keyboard callback for OpenGL
-//---------------------------------------
-void keyboard(unsigned char key, int x, int y)
-{
-   // End program
-   if (key == 'q')
-      exit(0);
+// Set up the rays to point from the camera to the projection plane
+void InitRays() {
+  Point3D camera(0, 0, -1 * distance);
 
-   // TO BE ADDED
+  rays.reserve(kX * kY);
+  for (int row = 0; row < kX; row++) {
+    for (int col = 0; col < kY; col++) {
+      //std::cout << "(" << kX*row+col << ") row: " << row << "col: " << col << std::endl;
+      std::pair<int, int> world_x_y = ImgToWorld(row, col);
+      Point3D projection_point(world_x_y.first, world_x_y.second, kProjectionPlaneDistance);
 
-   // Display image
-   glutPostRedisplay();
+      rays[kX*row + col] = Ray3D(camera, projection_point);
+    }
+  }
 }
 
-//---------------------------------------
-// Main program
-//---------------------------------------
-int main(int argc, char *argv[])
-{
-   // Create OpenGL window
-   glutInit(&argc, argv);
-   glutInitWindowSize(kX, kY);
-   glutInitWindowPosition(0, 0);
-   glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-   glutCreateWindow("Ray Trace");
-   init();
+void display() {
+  // TO BE ADDED
 
-   // Specify callback function
-   glutDisplayFunc(display);
-   glutKeyboardFunc(keyboard);
-   glutMainLoop();
-   return 0;
+  // Display image
+  glClear(GL_COLOR_BUFFER_BIT);
+  glDrawPixels(kX, kY, GL_RGB, GL_UNSIGNED_BYTE, image);
+  glFlush();
+}
+
+
+void keyboard(unsigned char key, int x, int y) {
+  // End program
+  if (key == 'q') {
+    exit(0);
+  }
+
+  // TO BE ADDED
+
+  // Display image
+  glutPostRedisplay();
+}
+
+int main(int argc, char *argv[]) {
+  // Create OpenGL window
+  glutInit(&argc, argv);
+  glutInitWindowSize(kX, kY);
+  glutInitWindowPosition(0, 0);
+  glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+  glutCreateWindow("Ray Trace");
+  init();
+
+  InitRays();
+
+  // Specify callback function
+  glutDisplayFunc(display);
+  glutKeyboardFunc(keyboard);
+  glutMainLoop();
+  return 0;
 }
