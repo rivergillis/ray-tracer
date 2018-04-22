495dd6ef58e8ead87678c5be31cf2f03cf7ee007
River Gillis <jrivergillis@gmail.com>
2018-04-22 16:12:43 -0500

    Remove dbg code and remove outer ring of specular light on sphere

--- phong.cpp
+++ phong.cpp
@@ -28,18 +28,12 @@ void Phong::SetObject(Rgb color, double a, double d, double s, double alpha) {
 
 Rgb Phong::GetAmbient() const {
   Rgb color = light_color_.Multiply(object_color_);
-  if (constants::kDebug) {
-    std::cout << "\tAmbient: " << color.Multiply(object_a_).ToString() << std::endl;
-  }
   return color.Multiply(object_a_);
 }
 
 Rgb Phong::GetDiffuse(Vec3D normal) const {
   double dotted = light_direction_.Dot(normal);
   if (dotted < 0) {
-    if (constants::kDebug) {
-      std::cout << "\tdiffuse ret 0" << std::endl;
-    }
     return {0, 0, 0};
   }
   Rgb color = light_color_.Multiply(object_color_);
@@ -49,9 +43,6 @@ Rgb Phong::GetDiffuse(Vec3D normal) const {
 Vec3D Phong::GetPerfectReflection(Vec3D normal) const {
   double dotted = light_direction_.Dot(normal);
   if (dotted < 0) {
-    if (constants::kDebug) {
-      std::cout << "\tperfrefl ret 0" << std::endl;
-    }
     return {0, 0, 0};
   }
   return normal.Multiply(2.0 * dotted).Subtract(light_direction_);
@@ -63,26 +54,14 @@ Rgb Phong::GetSpecular(Point3D location, Vec3D normal) const {
   Vec3D R = GetPerfectReflection(normal).Normalized();
   double dotted = R.Dot(V);
   if (dotted < 0) {
-    if (constants::kDebug) {
-    std::cout << "\tspec ret 0" << std::endl;
-      return {0, 0, 0};
-    }
-  }
-
-  if (constants::kDebug) {
-    std::cout << "\tV: " << V.ToString() << std::endl;
-    std::cout << "\tR: " << R.ToString() << std::endl;
-    std::cout << "\tR dot V: " << dotted << std::endl;
-    std::cout << "\t(R dot V)^alpha: " << std::pow(dotted, object_alpha_) << std::endl;
+    return {0, 0, 0};
   }
 
   return light_color_.Multiply(std::pow(dotted, object_alpha_) * object_s_);
 }
 
 Rgb Phong::GetShade(Point3D location, Vec3D normal) const {
-  if (constants::kDebug) {
-    std::cout << "P[objcol: " << object_color_.ToString() << " objA: " << object_a_ << " objD: " << object_d_ << " objS " << object_s_ << " objAlpha: " << object_alpha_ << " loc: " << location.ToString() << " norm: " << normal.ToString() << " camloc: " << cam_location_.ToString() << " lightdir: " << light_direction_.ToString() << " lightcol " << light_color_.ToString() << std::endl; 
-  }
+  normal = normal.Normalized();
   return GetAmbient().Add(GetDiffuse(normal))
     .Add(GetSpecular(location, normal));
 }
